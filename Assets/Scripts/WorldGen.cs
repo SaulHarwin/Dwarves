02a413971df05dd137605491152bf4089f392890
Erik Flynn <15ebbolumburu@uckfield.college>
2021-03-17 20:32:20 +0000

    Tiles -> Texture
    Instead of creating near 1 million new game objects with sprite renderers, it creates a dynamic texture and sets it to a quad.

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -22,12 +22,21 @@ public class WorldGen : MonoBehaviour
     public Sprite highGrassSprite;
     public Sprite mountainSprite;
 
+    public Color deepWaterColor;
+    public Color waterColor;
+    public Color sandColor;
+    public Color grassColor;
+    public Color highGrassColor;
+    public Color mountainColor;
+
     public TerrainType[] terrainTypes;
 
     List<GridSpace> gridspaces = new List<GridSpace>();
 
     public bool useRandomSeed;
     public int seed;
+
+    Texture2D worldTexture;
 
     private void Start()
     { 
@@ -40,7 +49,11 @@ public class WorldGen : MonoBehaviour
 
     void InitializeWorld()
     {
-
+        worldTexture = new Texture2D(width, height);
+        GameObject worldDisplay = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        worldDisplay.GetComponent<MeshRenderer>().material.mainTexture = worldTexture;
+        worldDisplay.transform.localScale = new Vector3(width, height, 1);
+        worldDisplay.transform.SetParent(GameObject.Find("Grid Manager").transform);
         if (useRandomSeed)
         {
             seed = Random.Range(0, 100000);
@@ -50,13 +63,12 @@ public class WorldGen : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 GridSpace gs = new GridSpace();
-                gs.GSGO = new GameObject();
-                gs.GSGO.name = "Grid Space [" + x + ", " + y + "]";  
-                gs.GSGO.AddComponent<SpriteRenderer>();
-                gs.GSGO.transform.position = new Vector2((float)x / 10, (float)y / 10);
+               
+               
+               // gs.GSGO.name = "Grid Space [" + x + ", " + y + "]";  
+                //gs.GSGO.AddComponent<SpriteRenderer>();
+                //gs.GSGO.transform.position = new Vector2((float)x, (float)y);
                 gs.pos = new Vector2(x, y);
-                gs.GSGO.transform.localScale = new Vector2(0.1f, 0.1f);
-                gs.GSGO.transform.SetParent(GameObject.Find("Grid Manager").transform);
 
                 float sample = Mathf.PerlinNoise((seed + x) * scale, (seed + y) * scale);
                 sample *= amplitude;
@@ -91,6 +103,37 @@ public class WorldGen : MonoBehaviour
 
     void updateWorld()
     {
+        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GridSpace gs = returnGS(x, y);
+                switch (gs.terrainTypeID)
+                {
+                    case 0:
+                        worldTexture.SetPixel(x, y, mountainColor);
+                        break;
+                    case 1:
+                        worldTexture.SetPixel(x, y, highGrassColor);
+                        break;
+                    case 2:
+                        worldTexture.SetPixel(x, y, grassColor);
+                        break;
+                    case 3:
+                        worldTexture.SetPixel(x, y, sandColor);
+                        break;
+                    case 4:
+                        worldTexture.SetPixel(x, y, waterColor);
+                        break;
+                    case 5:
+                        worldTexture.SetPixel(x, y, deepWaterColor);
+                        break;
+                }
+            }
+            worldTexture.Apply();
+        }
+        /*
         foreach(GridSpace gs in gridspaces)
         {
             switch (gs.terrainTypeID)
@@ -115,18 +158,29 @@ public class WorldGen : MonoBehaviour
                     break;
             }
         }
+        */
     }
 
     float DistFromCentre(GridSpace gs)
     {
         float centreX = Mathf.Ceil(width / 2);
         float centreY = Mathf.Ceil(height / 2);
-        float distance = Vector2.Distance(new Vector2(centreX, centreY), gs.pos) / 6.5f;
-        Debug.Log(distance);
+        float widthHeight = centreX / centreY;
+        float distance = Vector2.Distance(new Vector2(centreX, centreY), gs.pos) / 5;
         return distance;
     }
 
-
+    public GridSpace returnGS(int x, int y)
+    {
+        foreach(GridSpace gs in gridspaces)
+        {
+            if (gs.pos == new Vector2(x, y))
+            {
+                return gs;
+            }
+        }
+        return null;
+    }
 }
 
 public class GridSpace
@@ -134,6 +188,12 @@ public class GridSpace
     public GameObject GSGO;
     public Vector2 pos;
     public int terrainTypeID;
+}
+
+public class Chunk
+{
+    public List<GridSpace> GridSpacesInChunk;
+    public Vector2 pos;
 }
 
 [System.Serializable]
